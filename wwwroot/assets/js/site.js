cc3a4e26ba5e417a01ec6757f0f6e118baa7872a
daveyedgar <62836024+daveyedgar@users.noreply.github.com>
2023-05-08 16:33:47 -0400

    fixed navbar

--- wwwroot/assets/js/site.js
+++ wwwroot/assets/js/site.js
@@ -1,25 +1,25 @@
-﻿document.getElementById("monthly").addEventListener("change", getYearly);
-document.getElementById("yearly").addEventListener("change", getMonthly);
-document.getElementById("btnReset").addEventListener("click", reset);
+﻿//document.getElementById("monthly").addEventListener("change", getYearly);
+//document.getElementById("yearly").addEventListener("change", getMonthly);
+//document.getElementById("btnReset").addEventListener("click", reset);
 
-function reset() {
-    document.querySelectorAll("input").forEach((input) => {
-        input.value = "";
-    })
-    //window.location.reload();
-    window.location = window.location
-};
+//function reset() {
+//    document.querySelectorAll("input").forEach((input) => {
+//        input.value = "";
+//    })
+//    //window.location.reload();
+//    window.location = window.location
+//};
 
-function getMonthly() {
-    let monthly = document.getElementById("monthly");
-    let yearly = document.getElementById("yearly");
-    yearly.focus();
-    monthly.value = yearly.value * 12;
-}
+//function getMonthly() {
+//    let monthly = document.getElementById("monthly");
+//    let yearly = document.getElementById("yearly");
+//    yearly.focus();
+//    monthly.value = yearly.value * 12;
+//}
 
-function getYearly() {
-    let monthly = document.getElementById("monthly");
-    let yearly = document.getElementById("yearly");
-    monthly.focus();
-    yearly.value = (monthly.value / 12).toFixed(2);
-}
+//function getYearly() {
+//    let monthly = document.getElementById("monthly");
+//    let yearly = document.getElementById("yearly");
+//    monthly.focus();
+//    yearly.value = (monthly.value / 12).toFixed(2);
+//}
